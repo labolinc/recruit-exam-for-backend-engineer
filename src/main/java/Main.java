07d04f14d99f8a8a1ee0a79b1b548cf6df0df01c
n-machida <n-machida@ceres-inc.jp>
2020-11-24 21:10:59 +0900

    initialize project

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * MySQLの接続可否確認
+         */
+        long count = DriverManager.drivers().count();
+        System.out.println("ドライバーの数: " + count);
+        System.out.println("ドライバー一覧: " + DriverManager.drivers().map(Object::toString).collect(Collectors.joining()));
+
+        String url = "jdbc:mysql://localhost:13306/moppy_test";
+        String user = "guest_user";
+        String password = "guest_password";
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("DBコネクション: " + connection);
+            System.out.println("接続成功");
+        } catch (SQLException e) {
+            System.out.println("MySQLの起動、MySQLのドライバーのインストール、MySQLの接続情報の3点を確認してください");
+            e.printStackTrace();
+        }
+    }
+}
